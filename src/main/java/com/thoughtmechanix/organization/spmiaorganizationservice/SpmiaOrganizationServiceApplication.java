b9e6120fd656024d1844fbec84975cec87c181b7
Abel Fernandez <abelfergodevelop@gmail.com>
2019-06-24 23:16:20 +0200

    Page 239

--- src/main/java/com/thoughtmechanix/organization/spmiaorganizationservice/SpmiaOrganizationServiceApplication.java
+++ src/main/java/com/thoughtmechanix/organization/spmiaorganizationservice/SpmiaOrganizationServiceApplication.java
@@ -3,9 +3,15 @@ package com.thoughtmechanix.organization.spmiaorganizationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableEurekaClient
 //@EnableFeignClients
+@EnableResourceServer
 @SpringBootApplication
 public class SpmiaOrganizationServiceApplication {
 
@@ -13,4 +19,9 @@ public class SpmiaOrganizationServiceApplication {
 		SpringApplication.run(SpmiaOrganizationServiceApplication.class, args);
 	}
 
+	@Bean
+    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
+        return new OAuth2RestTemplate(details, oauth2ClientContext);
+    }
+	
 }
